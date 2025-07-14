3dddbd962d6e6aa3a25d5989ab570a1d4b4b6aa5
raghu007-g <ragunoob308@gmail.com>
2025-07-14 09:44:33 +0530

    Add files via upload

---
+++ check positive or negative.py
@@ -0,0 +1,8 @@
+print("for checking positive or negtive")
+num = int(input("enter a number:").strip())
+if num == 0:
+    print("it is zero")
+elif num > 0:
+    print("it is positive")
+else:
+    print("it is negative")
