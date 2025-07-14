b8bb6123f9d0f74d005a9bcfcaddb0cb2c4bb07a
raghu007-g <ragunoob308@gmail.com>
2025-07-14 21:40:23 +0530

    Add files via upload
    using for loop print multiplication table

---
+++ multiplication table genrator.py
@@ -0,0 +1,8 @@
+print("multiplication table generator")
+num=int(input("enter a number:").strip())
+for i in range(1,11):
+        print(f"{num} * {i} = {num * i}")
+
+else:
+        print("for generating multiplication table")
+
